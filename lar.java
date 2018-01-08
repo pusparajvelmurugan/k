4b8b2e8f91d95c7526a1a282571456f49d94eaeb
pusparajvelmurugan <30748421+pusparajvelmurugan@users.noreply.github.com>
2018-01-08 16:50:03 +0530

    Update lar.java

--- lar.java
+++ lar.java
@@ -1,16 +1,15 @@
 public class NumberDigits {
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
 
-        int count = 0, num = 3452;
+int count = 0, num = 3452;
 
-        while(num != 0)
-        {
-            // num = num/10
-            num /= 10;
-            ++count;
-        }
-
-        System.out.println("Number of digits: " + count);
-    }
+while(num != 0)
+{
+            // num = num/
+num /= 10;
+++count;        
+}
+System.out.println("Number of digits: " + count);
+}
 }
